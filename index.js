821d452519021ca6bc85b6dfeae3724b396938fb
faysalahmed000012 <faysal000012@gmail.com>
2022-05-24 15:31:08 +0600

    jwt added

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorize access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.20nux.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -23,6 +38,7 @@ async function run() {
 
     const productCollection = client.db("electrofirm").collection("products");
     const OrderCollection = client.db("electrofirm").collection("orders");
+    const userCollection = client.db("electrofirm").collection("users");
 
     app.get("/products", async (req, res) => {
       const products = await productCollection.find().toArray();
@@ -43,6 +59,22 @@ async function run() {
       const result = await OrderCollection.insertOne(order);
       res.send(result);
     });
+
+    app.put("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, {
+        expiresIn: "2d",
+      });
+      res.send({ result, token });
+    });
   } finally {
   }
 }
@@ -52,5 +84,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+  console.log(`listening to port ${port}`);
 });
