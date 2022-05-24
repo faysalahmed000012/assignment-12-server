2bb62610a1c1464eca5da12a52a91ae1ffe34413
faysalahmed000012 <faysal000012@gmail.com>
2022-05-24 21:51:02 +0600

    product delete by id

--- index.js
+++ index.js
@@ -56,6 +56,13 @@ async function run() {
       res.send(product);
     });
 
+    app.delete("/product/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = productCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // post order
     app.post("/orders", async (req, res) => {
       const order = req.body;
