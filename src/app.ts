ecb5a9966e6e0f46423782f0257350df845ef602
faysalahmed000012 <faysal000012@gmail.com>
2024-06-29 22:26:16 +0600

    not sure what's happening

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
+import router from "./app/routes";
+
+const app: Application = express();
+
+// parsers
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("🤗 Welcome to Version 2");
+});
+
+// application routes
+app.use(router);
+
+// global Error Handler
+app.use(globalErrorHandler);
+
+// not found
+app.use(notFound);
+
+export default app;
